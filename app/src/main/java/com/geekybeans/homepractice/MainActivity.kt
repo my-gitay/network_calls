3fb68eacfc50a16ae567b82252417d3ec61a08d9
Big cheese <contact.itay@gmail.com>
2020-05-09 14:58:53 +0300

    first commit

---
+++ app/src/main/java/com/geekybeans/homepractice/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.geekybeans.homepractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+/** This is an home assignment that I created for practice **/
+/** The following todo's needs to be implemented: **/
+
+//TODO: fetch a json object from URL
+//TODO: parse that json object into a list
+//TODO: put that list inside a recycler view
+//TODO: the recycler view should have 2 view holders according to the data of an item
+//TODO: the recycler view should be in a fragment
+//TODO: on item click you should navigate to another fragment for presenting the data pressed
+//TODO: the app should handle orientation change
+//TODO: use MVVM pattern in your code
+//TODO: use Coroutines and LiveData
+//TODO: create a singleton
+
+class MainActivity : AppCompatActivity()
+{
+    private val viewModel: ActivityViewModel by viewModels()
+    private val stringToShow = "Stringgg"
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        if (supportFragmentManager.findFragmentByTag("tag2") == null)
+        {
+            supportFragmentManager.beginTransaction().add(R.id.fragment_container, FirstFragment()).commit()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle)
+    {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.putString("key", stringToShow)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        savedInstanceState.getString("key")
+    }
+
+}
+
+
